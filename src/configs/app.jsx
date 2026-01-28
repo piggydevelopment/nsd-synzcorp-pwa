1cb3e6459adf095ab0fa8580a8c022b64022d38b
Apinan Woratrakun <iamapinan@gmail.com>
2026-01-28 18:36:56 +0700

    fix: Update default API and CS URLs from localhost to `http://api.synzofficial.com`.

--- src/configs/app.jsx
+++ src/configs/app.jsx
@@ -1,10 +1,10 @@
 export const apiUrl =
-  process.env.REACT_APP_API_URL || "http://localhost:3000";
+  process.env.REACT_APP_API_URL || "http://api.synzofficial.com";
 export const apiToken = "";
 export const cs_token = "yoJJA4KDq7gBdYHN9AUtL6iU";
 export const cs_identifier = "YeLJZE6Pa9AvzSicdoeuseWV";
 export const cs_hmac_secret = "jb1mtajroy64cp3wjaxnagsxs2lr4j8t";
 export const app_name = "Synz Corporate";
 // export const cs_token = 'dvnz1PDNigwetK24P9WoeMqF';
-export const cs_url = process.env.REACT_APP_API_URL || "http://localhost:3000";
+export const cs_url = process.env.REACT_APP_API_URL || "http://api.synzofficial.com";
 export const orgId = process.env.REACT_APP_ORG_ID || "egat2";
